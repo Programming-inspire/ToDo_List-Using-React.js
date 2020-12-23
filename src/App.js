aa96c74f69b429dc20e8821a569b745538b94452
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2020-12-23 17:05:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DelUp from './components/DelUp';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <DelUp />
+    </div>
+  );
+}
+
+export default App;
